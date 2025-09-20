6b46e35262b722e8403b64c6523fab2c79061811
ArpanMahatra1999 <arpanmahatra1999ad@gmail.com>
2025-09-20 19:55:56 -0400

    removed cron and added workflow

--- cron.sh
+++
@@ -1,7 +0,0 @@
-# cron.sh
-#!/bin/bash
-set -e
-
-# Call inactive sessions every 15 minutes
-curl -H "X-API-KEY: $BACKEND_API_KEY" \
-     -X GET "https://patrolio-patrol-sessions.onrender.com/inactive_sessions/15"
